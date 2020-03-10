aadb5523fdea75d7df823b73e33da14cb4cb39a5
h01m3s <lin426797@gmail.com>
2020-03-10 19:15:18 -0400

    Separate index and App logic, implement interactions with server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,72 @@
 import React from 'react';
+import BudgetList from './components/BudgetList.jsx';
+import axios from 'axios';
+import { errorHanlder } from '../../utils';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      budgets: []
+    };
+
+    this.handleBudgetCreate = this.handleBudgetCreate.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAllBudgets();
+  }
+
+  fetchAllBudgets() {
+    return axios
+      .get('/api/budget')
+      .then(data => {
+        if (data.status !== 200) throw data;
+        else this.setState({ budgets: data.data });
+        data.data.forEach(budget => {
+          console.log(budget);
+        });
+      })
+      .catch(errorHanlder);
+  }
+
+  handleBudgetCreate(budget) {
+    return axios
+      .post('/api/budget', budget)
+      .then(data => {
+        if (data.status !== 201) throw data;
+        else this.fetchAllBudgets();
+      })
+      .catch(errorHanlder);
+  }
+
+  handleDeleteBudget(budget) {
+    return axios
+      .delete(`/api/budget/${budget._id}`)
+      .then(data => {
+        console.log(`${budget.name} deleted...`);
+        this.fetchAllBudgets();
+      })
+      .catch(errorHanlder);
+  }
+
+  handleUpdateBudget(budget) {
+    return axios
+      .put(`/api/budget/${budget._id}`, { data: budget })
+      .then(data => {
+        console.log(`${budget.name} deleted...`);
+        this.fetchAllBudgets();
+      })
+      .catch(errorHanlder);
   }
 
   render() {
-    return <h1>Hello from react...</h1>;
+    return (
+      <div>
+        <h1>Budget App</h1>
+        <BudgetList budgets={this.state.budgets} />
+      </div>
+    );
   }
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,49 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import BudgetList from "./components/BudgetList.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      budgets: [
-        {
-          name: "CHRISTMAS",
-          budget: 300,
-          description: "presents",
-          transactions: [
-            {
-              date: "Today",
-              description: "I want to buy a big santa",
-              amount: 500,
-              category: "Holidays"
-            }
-          ]
-        },
-        {
-          name: "Birthday",
-          budget: 200,
-          description: "presents",
-          transactions: []
-        },
-        {
-          name: "Wedding",
-          budget: 1000,
-          description: "gifts",
-          transactions: []
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Budget App</h1>
-        <BudgetList budgets={this.state.budgets} />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
