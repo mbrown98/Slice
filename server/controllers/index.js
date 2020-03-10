704b2f5fc3f12ce5f222d7c828c7ef3ef32aedc2
h01m3s <lin426797@gmail.com>
2020-03-10 15:31:34 -0400

    Implement getAllBudgets and sort by time

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,16 @@
+const Budget = require('../../database');
+
 module.exports = {
   getAllBudgets: (req, res) => {
-    res.send('get all budgets');
+    Budget.find()
+      .sort({ 'timestamps.createdAt': -1, 'timestamps.updatedAt': -1 })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(`err when get all budgets... ${err}`);
+        res.sendStatus(500);
+      });
   },
   createBudget: (req, res) => {
     res.send('post new budget');
