f14a20d098ccaedbc5a1f2e5edc402ad3bdbd676
h01m3s <lin426797@gmail.com>
2020-03-12 15:18:16 -0400

    Add PieChart

--- client/src/components/BudgetRow.jsx
+++ client/src/components/BudgetRow.jsx
@@ -18,6 +18,97 @@ class BudgetRow extends React.Component {
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
   }
 
+  updateChart() {
+    console.log("here....");
+    const jsonData = this.state.budget.transactions.reduce((acc, cur) => {
+      const index = acc.map(item => item.category).indexOf(cur.category);
+      console.log(`index: ${index}`);
+      if (index === -1) {
+        acc.push({ category: cur.category, count: 1 });
+      } else {
+        acc[index].count = acc[index].count + 1;
+      }
+      return acc;
+    }, []);
+    const totalCount = jsonData.reduce((acc, cur) => {
+      return (acc += cur.count);
+    }, 0);
+    var svg = d3.select(`[id="${this.state.budget._id}"]`),
+      width = svg.attr("width"),
+      height = svg.attr("height"),
+      radius = Math.min(width, height) / 2;
+
+    var g = svg
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal([
+      "gray",
+      "green",
+      "brown",
+      "orange",
+      "yellow",
+      "red",
+      "purple"
+    ]);
+
+    var pie = d3.pie().value(function(d) {
+      return d.count;
+    });
+
+    var path = d3
+      .arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+    var label = d3
+      .arc()
+      .outerRadius(radius)
+      .innerRadius(radius - 200);
+
+    var arc = g
+      // .select(`[id="${this.state.budget._id}"]`)
+      .selectAll(".arc")
+      .data(pie(jsonData))
+      .enter()
+      .append("g")
+      .attr("class", "arc");
+
+    arc
+      .append("path")
+      .attr("d", path)
+      .attr("fill", function(d) {
+        return color(d.index);
+      });
+
+    arc
+      .append("text")
+      .attr("transform", function(d) {
+        return "translate(" + label.centroid(d) + ")";
+      })
+
+      .text(function(d) {
+        return `${
+          d.data.category
+        }: ${((d.data.count / totalCount).toFixed(2) * 100).toFixed(1)}%`;
+      });
+
+    svg
+      .append("g")
+      .attr("transform", "translate(" + (width / 2 - 200) + "," + 25 + ")")
+      .append("text")
+      .text("Categories")
+      .attr("class", "title");
+  }
+
+  componentDidUpdate() {
+    this.updateChart();
+  }
+
+  componentDidMount() {
+    this.updateChart();
+  }
+
   handleChange(event) {
     const currentBudget = {
       ...this.state.budget,
@@ -51,7 +142,7 @@ class BudgetRow extends React.Component {
 
   render() {
     return (
-      <div id="BudgetRow" className="card box">
+      <div id="BudgetRow" className="box">
         <div className="buttons is-pulled-right">
           <button className="button is-primary" onClick={this.handleEdit}>
             Edit
@@ -117,33 +208,41 @@ class BudgetRow extends React.Component {
             </div>
           </div>
 
-          <div>
-            <h4>Transactions</h4>
-            <button
-              className="button is-primary"
-              onClick={() =>
-                this.setState({ addTransClicked: !this.state.addTransClicked })
-              }
-            >
-              Add Transaction
-            </button>
-            {this.state.addTransClicked && (
-              <TransactionAdd addNewTrans={this.addNewTransaction} />
-            )}
-            <table className="table is-striped">
-              <thead>
-                <tr>
-                  <th>Description</th>
-                  <th>Amount</th>
-                  <th>Category</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.budget.transactions.map(trans => (
-                  <Transactions key={trans._id} transaction={trans} />
-                ))}
-              </tbody>
-            </table>
+          <div className="columns">
+            <div className="column">
+              <h4>Transactions</h4>
+              <button
+                className="button is-primary"
+                onClick={() =>
+                  this.setState({
+                    addTransClicked: !this.state.addTransClicked
+                  })
+                }
+              >
+                Add Transaction
+              </button>
+              {this.state.addTransClicked && (
+                <TransactionAdd addNewTrans={this.addNewTransaction} />
+              )}
+              <table className="table is-striped">
+                <thead>
+                  <tr>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.budget.transactions.map(trans => (
+                    <Transactions key={trans._id} transaction={trans} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="column">
+              <svg id={this.state.budget._id} width="400" height="400"></svg>
+            </div>
           </div>
         </div>
       </div>
